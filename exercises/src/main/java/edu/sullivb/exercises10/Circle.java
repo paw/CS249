0f49ee0016c602e17d0f504fdce889eda6c48863
Briar <51191974+paw@users.noreply.github.com>
2024-02-13 11:50:02 -0500

    circle exercise con't

--- exercises/src/main/java/edu/sullivb/exercises10/Circle.java
+++ exercises/src/main/java/edu/sullivb/exercises10/Circle.java
@@ -2,9 +2,21 @@ package edu.sullivb.exercises10;
 import edu.sullivb.exercises09.Matrix;
 
 public class Circle {
+    // Data w/ Defaults
     private double radius = 1.0;
     private Matrix pos = Matrix.makePoint2D(0,0);
 
+    // Constructors
+    public Circle() {} // creates a unit circle at 0,0
+    public Circle(double r) {
+        setRadius(r);
+    }
+    public Circle(double r, Matrix c) {
+        setRadius(r);
+        setCenter(c);
+    }
+
+    // Getters & Setters
     public double getRadius() {
         return radius;
     }
@@ -19,10 +31,11 @@ public class Circle {
     public void setCenter(Matrix c) {
         pos = new Matrix(c);
     }
-
     public double getArea() {
         return Math.PI * radius * radius;
     }
+
+    // to Strings
     public String toString() {
         String s = "(Circle)\n";
         s += "radius: " + radius + "\n";
--- exercises/src/main/java/edu/sullivb/exercises10/CircleLand.java
+++ exercises/src/main/java/edu/sullivb/exercises10/CircleLand.java
@@ -7,5 +7,7 @@ public class CircleLand {
         Matrix p = c.getCenter();
         p.set(0,0,27);
         System.out.println(c);
+        Circle c2 = new Circle(5.6, Matrix.makePoint2D(2,-3));
+        System.out.println(c2);
     }
 }
